8cf97df5f84d548f46398d8d19a215c2cd5e5bd7
叶桐 <1766468434@qq.com>
2025-06-27 00:44:24 +0800

    fix:优化AI提示词

--- server/config/prompt-templates.js
+++ server/config/prompt-templates.js
@@ -6,7 +6,7 @@
  */
 const promptTemplates = {
   // 通用对话模板
-  outsourcing: `你是一个判断是否为外包公司的AI，请判断公司：{{query}}  是否为外包公司，并返回true或false`,
+  outsourcing: `你是一个判断是否为外包公司的AI，请判断公司：{{query}}  是否为外包公司，如果是外包公司则返回true，不是则返回false，严禁返回其他内容`,
 };
 
 export default promptTemplates; 
